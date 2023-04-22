cc963cceefec4dada7567ca8d483b884a7dc208a
Felix Palmen <felix@palmen-it.de>
2023-04-22 11:44:58 +0200

    Complete wiring of bidirectional connections

--- src/bin/tlsc/tlsc.c
+++ src/bin/tlsc/tlsc.c
@@ -1,8 +1,13 @@
+#include "client.h"
+#include "clientopts.h"
 #include "config.h"
+#include "connection.h"
 #include "event.h"
 #include "daemon.h"
 #include "daemonopts.h"
 #include "log.h"
+#include "server.h"
+#include "serveropts.h"
 #include "service.h"
 #include "threadopts.h"
 #include "threadpool.h"
@@ -39,20 +44,98 @@ static ThreadOpts threadOpts = {
 };
 
 static const Config *cfg;
+static Server *server = 0;
 
-static void startup(void *receiver, void *sender, void *args)
+static void datareceived(void *receiver, void *sender, void *args)
+{
+    (void)sender;
+
+    Connection *c = receiver;
+    DataReceivedEventArgs *ea = args;
+    ea->handling = 1;
+    Connection_write(c, ea->buf, ea->size, ea);
+}
+
+static void datasent(void *receiver, void *sender, void *args)
+{
+    (void)sender;
+    (void)args;
+
+    Connection *c = receiver;
+    Connection_confirmDataReceived(c);
+}
+
+static void connected(void *receiver, void *sender, void *args)
+{
+    (void)args;
+
+    Connection *cl = receiver;
+    Connection *sv = sender;
+
+    Event_register(Connection_dataReceived(cl), sv, datareceived, 0);
+    Event_register(Connection_dataReceived(sv), cl, datareceived, 0);
+    Event_register(Connection_dataSent(cl), sv, datasent, 0);
+    Event_register(Connection_dataSent(sv), cl, datasent, 0);
+}
+
+static void connclosed(void *receiver, void *sender, void *args)
+{
+    (void)sender;
+    (void)args;
+
+    Connection *c = receiver;
+    Event_unregister(Connection_closed(c), sender, connclosed, 0);
+    Connection_close(c, 0);
+}
+
+static void newclient(void *receiver, void *sender, void *args)
+{
+    (void)receiver;
+    (void)sender;
+
+    Connection *cl = args;
+
+    ClientOpts co = { 0 };
+    co.remotehost = cfg->remotehost;
+    co.port = cfg->remoteport;
+    co.numerichosts = 1;
+    co.tls = 1;
+    Connection *sv = Connection_createTcpClient(&co);
+
+    Event_register(Connection_closed(cl), sv, connclosed, 0);
+    Event_register(Connection_closed(sv), cl, connclosed, 0);
+    Event_register(Connection_connected(sv), cl, connected, 0);
+}
+
+static void svstartup(void *receiver, void *sender, void *args)
 {
     (void)receiver;
     (void)sender;
 
     StartupEventArgs *ea = args;
+
+    ServerOpts so = { 0 };
+    so.bindhost[0] = cfg->bindhost;
+    so.port = cfg->bindport;
+    so.numerichosts = 1;
+    server = Server_createTcp(&so);
+    if (!server)
+    {
+	ea->rc = EXIT_FAILURE;
+	return;
+    }
+
+    Event_register(Server_clientConnected(server), 0, newclient, 0);
 }
 
-static void shutdown(void *receiver, void *sender, void *args)
+static void svshutdown(void *receiver, void *sender, void *args)
 {
     (void)receiver;
     (void)sender;
     (void)args;
+
+    Server_destroy(server);
+    server = 0;
 }
 
 static void daemonized(void)
@@ -69,8 +152,8 @@ static int daemonrun(void *data)
     if (Service_init(&daemonOpts) >= 0)
     {
 	Service_setTickInterval(1000);
-	Event_register(Service_startup(), 0, startup, 0);
-	Event_register(Service_shutdown(), 0, shutdown, 0);
+	Event_register(Service_startup(), 0, svstartup, 0);
+	Event_register(Service_shutdown(), 0, svshutdown, 0);
 	if (ThreadPool_init(&threadOpts) >= 0)
 	{
 	    rc = Service_run();
@@ -85,6 +168,9 @@ static int daemonrun(void *data)
 SOLOCAL int Tlsc_run(const Config *config)
 {
     cfg = config;
+#ifdef DEBUG
+    Log_setMaxLogLevel(L_DEBUG);
+#endif
     if (cfg->daemonize)
     {
 	Log_setSyslogLogger(LOGIDENT, LOG_DAEMON, 1);
